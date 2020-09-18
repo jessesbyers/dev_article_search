bb830cd691868a54f7ff1e3f62b4e201971fa26a
Jesse Byers <jesse.s.byers@gmail.com>
2020-09-17 20:30:15 -0400

    moved dev article fetch to search.py

---
+++ search.py
@@ -0,0 +1,22 @@
+import requests
+import config
+# import sys
+# from colorama import init, Fore
+
+# init(autoreset=True)
+
+headers = {
+    "api-key": config.DEV_API_KEY
+}
+
+url = "https://dev.to/api/articles/me/published"
+
+data = requests.get(url, headers=headers).json()
+print(data)
+
+article_list = []
+for article in data:
+    article_list.append(article["title"])
+
+print("_______________________________")
+print(article_list)
