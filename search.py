2af7f6e67408ab4693d2dbf7b3578b8276a6fe40
Jesse Byers <jesse.s.byers@gmail.com>
2020-09-18 20:40:50 -0400

    added search logic to search.py

--- search.py
+++ search.py
@@ -1,7 +1,12 @@
+# IMPORT DEPENDENCIES
 import requests
 import config
 
+from colorama import init, Fore
+init(autoreset=True)
 
+
+# FETCH ARTICLE TITLES FROM DEV.TO API
 headers = {
     "api-key": config.DEV_API_KEY
 }
@@ -17,7 +22,29 @@ for article in data:
 recent_articles = article_list[:10]
 
 
-# import sys
-# from colorama import init, Fore
+# SET UP QUERIES TO GOOGLE CUSTOM SEARCH ENGINE FOR EACH ARTICLE TITLE
+for i, article_title in enumerate(recent_articles):
+    print(Fore.GREEN + str(i+1) + ". " + article_title.upper())
 
-# init(autoreset=True)
+    url = f"https://www.googleapis.com/customsearch/v1?key={config.GOOGLE_API_KEY}&cx={config.SEARCH_ENGINE_ID}&q={article_title}&start=1"
+
+    data = requests.get(url).json()
+
+    try:
+        hits = data.get("items")[:10]
+
+        for i, hit in enumerate(hits, start=1):
+            title = hit.get("title")
+            snippet = hit.get("snippet")
+            link = hit.get("link")
+            print("="*10, f"Result #{i}", "="*10)
+            print("Title:", title)
+            print("Description:", snippet)
+            print("URL:", Fore.BLUE + link, "\n")
+
+    except:
+        print(Fore.RED + "No search results available.")
+
+
+
+
