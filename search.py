1ab34da05c6173cc0530e71067a14424421ab62a
Jesse Byers <jesse.s.byers@gmail.com>
2020-09-18 21:39:26 -0400

    added quotes around title for search query

--- search.py
+++ search.py
@@ -25,8 +25,9 @@ recent_articles = article_list[:10]
 # SET UP QUERIES TO GOOGLE CUSTOM SEARCH ENGINE FOR EACH ARTICLE TITLE
 for i, article_title in enumerate(recent_articles):
     print(Fore.GREEN + str(i+1) + ". " + article_title.upper())
+    title = '"' + article_title + '"'
 
-    url = f"https://www.googleapis.com/customsearch/v1?key={config.GOOGLE_API_KEY}&cx={config.SEARCH_ENGINE_ID}&q={article_title}&start=1"
+    url = f"https://www.googleapis.com/customsearch/v1?key={config.GOOGLE_API_KEY}&cx={config.SEARCH_ENGINE_ID}&q={title}&start=1"
 
     data = requests.get(url).json()
 
